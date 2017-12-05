68d371e764e3f8b9af49693c2ef6120f534b190f
raquelvilione <raquel_vilione458@hotmail.com>
2017-12-05 16:53:57 +0000

    Commit 7 -> Usuario.hs

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -4,17 +4,25 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies, DeriveGeneric #-}
 module Handler.Usuario where
-
+-- ----------------------------------------------------------------------------------------------------------------------
+-- IMPORT
+-- ----------------------------------------------------------------------------------------------------------------------
 import Import
--- import Network.HTTP.Types.Status
+import Network.HTTP.Types.Status
 import Data.Aeson.Types
 import Database.Persist.Postgresql
-
+-- ----------------------------------------------------------------------------------------------------------------------
+-- POST
+-- ----------------------------------------------------------------------------------------------------------------------
 postUsuarioR :: Handler Value
 postUsuarioR = do
-    usu <- requireJsonBody :: Handler Usuario
-    usuid <- runDB $ insert usu
-    sendStatusJSON created201 (object ["resp" .= (fromSqlKey usuid)])
-
+    usu <- requireJsonBody :: Handler Usuario -- mandar vazio
+    let hash = (usuarioNome usu ++ usuarioSenha usu)
+    let usuario = Usuario (usuarioNome usu) (usuarioSobrenome usu) (usuarioEmail usu) (usuarioSenha usu) (hash)
+    usuarioId <- runDB $ insert usuario
+    sendStatusJSON created201 (object ["resp" .= hash ])
+-- ----------------------------------------------------------------------------------------------------------------------
+-- 
+-- ----------------------------------------------------------------------------------------------------------------------
 
     
